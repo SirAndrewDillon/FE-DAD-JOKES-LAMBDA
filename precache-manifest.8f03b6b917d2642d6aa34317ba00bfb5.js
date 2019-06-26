83cc136cf33faaaf9c12537ccc559fa2c9dbbbc5
Andrew Dillon <majik895@gmail.com>
2019-06-26 19:49:10 -0400

    Updates

---
+++ precache-manifest.8f03b6b917d2642d6aa34317ba00bfb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbb77aed2f6979bd462da784ff413fff",
+    "url": "/https://github.com/dadjokes-lambda-bw/dadjokesFE/index.html"
+  },
+  {
+    "revision": "1eaf357bd7403d07c270",
+    "url": "/https://github.com/dadjokes-lambda-bw/dadjokesFE/static/css/main.b0c6b8e4.chunk.css"
+  },
+  {
+    "revision": "9284d5b282edfcfde739",
+    "url": "/https://github.com/dadjokes-lambda-bw/dadjokesFE/static/js/2.6e653e07.chunk.js"
+  },
+  {
+    "revision": "1eaf357bd7403d07c270",
+    "url": "/https://github.com/dadjokes-lambda-bw/dadjokesFE/static/js/main.f87d0e3b.chunk.js"
+  },
+  {
+    "revision": "2c7b5456daae6e135002",
+    "url": "/https://github.com/dadjokes-lambda-bw/dadjokesFE/static/js/runtime~main.a37e901f.js"
+  }
+]);
