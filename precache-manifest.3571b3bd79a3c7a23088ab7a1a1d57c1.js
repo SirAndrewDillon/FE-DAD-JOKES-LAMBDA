9ac5fee5d930de72cd9cacc26b42d89a35de1006
Andrew Dillon <majik895@gmail.com>
2019-06-26 19:51:30 -0400

    Updates

---
+++ precache-manifest.3571b3bd79a3c7a23088ab7a1a1d57c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3624d01003600ea3d85dd198c0cb137f",
+    "url": "/https://github.com/https://github.com/dadjokes-lambda-bw/dadjokesFE/tree/dillon/index.html"
+  },
+  {
+    "revision": "1eaf357bd7403d07c270",
+    "url": "/https://github.com/https://github.com/dadjokes-lambda-bw/dadjokesFE/tree/dillon/static/css/main.b0c6b8e4.chunk.css"
+  },
+  {
+    "revision": "9284d5b282edfcfde739",
+    "url": "/https://github.com/https://github.com/dadjokes-lambda-bw/dadjokesFE/tree/dillon/static/js/2.6e653e07.chunk.js"
+  },
+  {
+    "revision": "1eaf357bd7403d07c270",
+    "url": "/https://github.com/https://github.com/dadjokes-lambda-bw/dadjokesFE/tree/dillon/static/js/main.f87d0e3b.chunk.js"
+  },
+  {
+    "revision": "1684936501dea1fb60a2",
+    "url": "/https://github.com/https://github.com/dadjokes-lambda-bw/dadjokesFE/tree/dillon/static/js/runtime~main.cfeada4c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/https://github.com/dadjokes-lambda-bw/dadjokesFE/precache-manifest.8f03b6b917d2642d6aa34317ba00bfb5.js"
+  "/https://github.com/https://github.com/dadjokes-lambda-bw/dadjokesFE/tree/dillon/precache-manifest.3571b3bd79a3c7a23088ab7a1a1d57c1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/https://github.com/dadjokes-lambda-bw/dadjokesFE/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/https://github.com/https://github.com/dadjokes-lambda-bw/dadjokesFE/tree/dillon/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
