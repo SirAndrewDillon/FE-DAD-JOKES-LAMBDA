96a7d685970e06c24a8f12d8c339e66a4f3e3f59
Andrew Dillon <majik895@gmail.com>
2019-06-26 22:48:25 -0400

    test

---
+++ build/precache-manifest.86dfdcd0a6215e7c6914051593067897.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "863c61245cd5775fd91578b808e74774",
+    "url": "/https://github.com/https://github.com/dadjokes-lambda-bw/dadjokesFE/tree/dillon/index.html"
+  },
+  {
+    "revision": "6cc812869c29d153ca99",
+    "url": "/https://github.com/https://github.com/dadjokes-lambda-bw/dadjokesFE/tree/dillon/static/css/main.b0c6b8e4.chunk.css"
+  },
+  {
+    "revision": "d444557612d146321a51",
+    "url": "/https://github.com/https://github.com/dadjokes-lambda-bw/dadjokesFE/tree/dillon/static/js/2.ad7958c2.chunk.js"
+  },
+  {
+    "revision": "6cc812869c29d153ca99",
+    "url": "/https://github.com/https://github.com/dadjokes-lambda-bw/dadjokesFE/tree/dillon/static/js/main.6d3aeb1b.chunk.js"
+  },
+  {
+    "revision": "1684936501dea1fb60a2",
+    "url": "/https://github.com/https://github.com/dadjokes-lambda-bw/dadjokesFE/tree/dillon/static/js/runtime~main.cfeada4c.js"
+  }
+]);
